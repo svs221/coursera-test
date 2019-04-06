78b1c0920fb083e0c25b820b51ebe3da319387d7
Skanda <songoku823@yahoo.com>
2019-04-06 09:36:39 -0400

    Assignment 1 Solution

---
+++ module1-solution/app.js
@@ -0,0 +1,47 @@
+(function () {
+'use strict';
+
+
+angular.module('LunchApp', [])
+
+.controller('LunchCheckController', function($scope){
+
+$scope.list = "";
+$scope.message = "";
+$scope.correctColor = "";
+$scope.borderColor = "grey";
+
+$scope.check = function(){
+
+  if($scope.list == ""){
+    $scope.correctColor = "red";
+    $scope.borderColor = "red"
+    $scope.message = "Please enter data first";
+
+  }
+  else{
+    $scope.correctColor = "green";
+    $scope.borderColor = "green";
+    var items = $scope.list.split(",");
+    var count = 0;
+    for(var i = 0; i < items.length; ++i){
+      if(items[i].trim() != ""){
+        count += 1;
+      }
+    }
+
+    if(count <= 3){
+      $scope.message = "Enjoy!";
+    }
+    else{
+      $scope.message = "Too much!";
+    }
+
+  }
+}
+
+
+});
+
+
+})();
